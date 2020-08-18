b992206988b12784d79beb940899bd0476430120
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-08-18 17:04:06 +0000

    completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Give me a store name "
+@user_input = gets.chomp.to_s
+puts @user_input
+
+new_store = Store.create(
+  name: @user_input
+)
+
+puts new_store.valid?
